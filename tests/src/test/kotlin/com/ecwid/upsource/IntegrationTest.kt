faeae745a7cb284bf8d438e7604014c4f2412b29
Alexander Turchenko <tilex@ecwid.com>
2020-05-24 18:31:38 +0400

    Extend integration test for all modules

--- tests/src/test/kotlin/com/ecwid/upsource/IntegrationTest.kt
+++ tests/src/test/kotlin/com/ecwid/upsource/IntegrationTest.kt
@@ -1,16 +1,19 @@
 package com.ecwid.upsource
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import com.ecwid.upsource.rpc.UpsourceRPC
+import com.ecwid.upsource.rpc.projects.ReviewDescriptorDTO
+import com.ecwid.upsource.rpc.projects.ReviewStateEnum
+import com.ecwid.upsource.rpc.projects.ReviewsRequestDTO
 import com.ecwid.upsource.rpc.projects.RevisionListDTO
-import com.ecwid.upsource.serializer.GsonSerializer
-import com.ecwid.upsource.transport.ApacheHttpRpcTransport
-import com.ecwid.upsource.transport.HttpRpcTransport
+import com.ecwid.upsource.serializer.gson.withGsonSerializer
+import com.ecwid.upsource.serializer.jakson.withJaksonSerializer
 import com.ecwid.upsource.transport.RpcResponse
 import com.ecwid.upsource.transport.UpsourceConnection
-import org.junit.jupiter.api.Disabled
+import com.ecwid.upsource.transport.apache.withApacheHttpClient
+import com.ecwid.upsource.transport.httpclient.withHttpClient
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-@Disabled
 internal class IntegrationTest {
 	private val upsourceConnection = UpsourceConnection(
 		serverUrl = System.getenv("UPSOURCE_URL"),
@@ -18,16 +21,19 @@ internal class IntegrationTest {
 		password = System.getenv("UPSOURCE_PASSWORD")
 	)
 
+	private val demoProjectId = System.getenv("DEMO_PROJECT_ID")
+
 	@Test
 	fun `Gson and java httpclient test`() {
-		val transport = HttpRpcTransport(upsourceConnection)
-		val serializer = GsonSerializer()
-		val client = ClientFactory.newUpsourceRPC(transport, serializer)
+		val client = UpsourceRPC.newBuilder()
+			.withHttpClient(upsourceConnection)
+			.withGsonSerializer()
+			.build()
 
 		val revisionList = client.getRevisionReviewInfo(
 			RevisionListDTO(
-				projectId = "project",
-				revisionId = listOf("revision_id")
+				projectId = "unknown-project",
+				revisionId = listOf("test-revision-id")
 			)
 		)
 
@@ -36,19 +42,87 @@ internal class IntegrationTest {
 	}
 
 	@Test
-	fun `Gson and apache httpclient test`() {
-		val transport = ApacheHttpRpcTransport(upsourceConnection)
-		val serializer = GsonSerializer()
-		val client = ClientFactory.newUpsourceRPC(transport, serializer)
+	fun `Jakson and apache httpclient test`() {
+		val client = UpsourceRPC.newBuilder()
+			.withApacheHttpClient(upsourceConnection)
+			.withJaksonSerializer()
+			.build()
 
 		val revisionList = client.getRevisionReviewInfo(
 			RevisionListDTO(
-				projectId = "project",
-				revisionId = listOf("revision_id")
+				projectId = "unknown-project",
+				revisionId = listOf("test-revision-id")
 			)
 		)
 
 		require(revisionList is RpcResponse.Error)
 		assertThat(revisionList.code).isEqualTo(2001)
+	}
+
+	@Test
+	fun `Fetch all opened reviews test`() {
+		val client = UpsourceRPC.newBuilder()
+			.withHttpClient(upsourceConnection)
+			.withJaksonSerializer()
+			.build()
+
+		val openedReviews = client
+			.fetchReviews(
+				project = demoProjectId,
+				query = "state: open"
+			)
+			.take(15)
+			.toList() // materialize
+
+		val closedReviews = client
+			.fetchReviews(
+				project = demoProjectId,
+				query = "state: closed"
+			)
+			.take(15)
+			.toList() // materialize
+
+		openedReviews.forEach {
+			require(it.state == ReviewStateEnum.OPEN)
+		}
+
+		closedReviews.forEach {
+			require(it.state == ReviewStateEnum.CLOSED)
+		}
+	}
+}
+
+/**
+ * Fetch reviews by query
+ */
+private fun UpsourceRPC.fetchReviews(project: String, query: String): Sequence<ReviewDescriptorDTO> {
+	var offset = 0
+	val limit = 10
+	val client = this
+	return sequence {
+		while (true) {
+			val request = ReviewsRequestDTO(
+				projectId = project,
+				query = query,
+				limit = limit,
+				skip = offset
+			)
+
+			when (val response = client.getReviews(request)) {
+				is RpcResponse.Ok -> {
+
+					yieldAll(response.result.reviews)
+					offset += limit
+
+					if (response.result.reviews.size < limit || response.result.reviews.isEmpty()) {
+						return@sequence
+					}
+				}
+
+				is RpcResponse.Error -> {
+					throw IllegalStateException("Response error: $response")
+				}
+			}
+		}
 	}
 }
